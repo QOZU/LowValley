e3307d69e41eac33e1a98b21a7510977c30ef377
QOZU <64510789+QOZU@users.noreply.github.com>
2025-08-05 20:30:24 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/lowvalley/main.go
@@ -0,0 +1,23 @@
+// cmd/lowvalley/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"lowvalley/internal/lowvalley"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := lowvalley.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
